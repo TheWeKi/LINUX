6e33af53bbc1bc6927228d3a5bcc888cd0e19b26
theweki <rp.weki.24@gmail.com>
2024-09-23 14:00:37 +0530

    ^i3

--- $1/SCRIPTS/i3.sh
+++ $1/SCRIPTS/i3.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
 pacman --noconfirm -S xorg xorg-xinit fwupd cups usbutils
-# pacman --noconfirm -S i3 polybar rofi alacritty ... 
+
 pacman --noconfirm -S noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd vlc
 pacman --noconfirm -S mesa vulkan-intel intel-media-driver libva-utils libva-intel-driver
 
 sleep 1
-
-# systemctl enable power-profiles-daemon.service
-systemctl enable gdm.service
-systemctl enable switcheroo-control.service
 
 systemctl enable cups.socket
 systemctl enable avahi-daemon.socket
