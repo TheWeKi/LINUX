d0e93a8d1800729a20c53f834a40e65071dd4356
theweki <rp.weki.24@gmail.com>
2024-10-09 05:25:51 +0530

    sfdisk

--- arch/install/sfdisk.sh
+++ arch/install/sfdisk.sh
@@ -4,16 +4,11 @@ lsblk
 
 read -p "DISK   |   nvme0n1 / sda / vda :   " DISK
 
-# Create GPT Partition Table with No Partitions
-echo 'label: gpt' | sfdisk /dev/$DISK
-
-# EFI Partition
-echo ',+1G,EF00,*' | sfdisk /dev/$DISK
-
-# SWAP Partition
-echo ',+16G,8200,' | sfdisk /dev/$DISK
-
-# ROOT Partition
-echo ',,8300,' | sfdisk /dev/$DISK
+{
+    echo "label: gpt"
+    echo ",+1G,EF00,*"  # EFI Partition
+    echo ",+16G,8200,"  # SWAP Partition
+    echo ",,"           # ROOT Partition
+} | sfdisk /dev/$DISK
 
 sleep 1
